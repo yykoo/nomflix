911a99d2aa4b07950a60586b43c10d1c2ce4ab6b
DESKTOP-OTAFVNA\iksou <iksoub@gmail.com>
2022-12-08 23:31:38 +0900

    Header 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./routes/Home";
+import Search from "./routes/Search";
+import Tv from './routes/Tv';
 
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={< Home />} />
+        <Route path="/tv" element={< Tv />} />
+        <Route path="/search" element={< Search />} />
+      </Routes>
+    </BrowserRouter>);
 }
 
 export default App;
